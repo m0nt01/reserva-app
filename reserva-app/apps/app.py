1aeb6e77155178aa3182b637ebea6fec8abebdbc
Guilherme Yoshida <guiyyoshida@gmail.com>
2024-07-31 22:53:15 -0300

    att

--- reserva-app/apps/app.py
+++ reserva-app/apps/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -39,3 +39,34 @@ def reservas():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
+def obter_salas():
+    with open("salas.csv", "r") as file:
+        lista_salas = []
+        for linha in file:
+            id, tipo, descricao, capacidade = linha.strip().split(",")
+            sala = {
+                "id": id,
+                "tipo": tipo,
+                "descricao": descricao,
+                "capacidade": int(capacidade),
+            }
+            lista_salas.append(sala)
+        
+        return lista_salas
+    
+def cadastrar_sala(sala):
+    linha = f"\n{sala['id']},{sala['tipo']},{sala['descricao']},{sala['capacidade']}"
+    with open("salas.csv", "a") as file:
+        file.write(linha)
+
+@app.route('/reservar-sala', methods=['POST'])
+def reservar_sala(reserva):
+    linha = f"\n{reserva['nome']},{reserva['data']},{reserva['sala']},{reserva['acoes']}"
+    with open("reservas.csv", "a") as file:
+        file.write(linha)
+    
+def cadastrar_usuario(usuario):
+    linha = f"\n{usuario['nome']},{usuario['email']},{usuario['senha']}"
+    with open("usuarios.csv", "a") as file:
+        file.write(linha)
